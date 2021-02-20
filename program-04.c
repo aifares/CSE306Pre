df0ddfb030a822a7dfda66d19d7dcff2d94da3e2
repl.it user <replituser@example.com>
2021-02-20 03:31:37 +0000

    fixed mean maybe

--- program-04.c
+++ program-04.c
@@ -10,9 +10,10 @@ char line[4096] = "";
 int rowCol = -1;
 int minimum = 1000000000;
 int maximum = -1000000000;
-float meanTotal = 0;
-int meanCount = 0;
-float mean = 0;
+double meanTotal = 0;
+double trueTemp = 0;
+int meanCount = -1;
+double mean = 0;
 int bufferChecker = 0;
 
 // This is a boolean to check for double quotes 0 = false 1 = true
@@ -48,103 +49,101 @@ int number_of_data(){
 }
 
 int minMaxMean(char* Header , int operation){
-
-  while ((ch = fgetc(filePointer)) != EOF)
-      {
-        //If we are in quotes add commas to string
-        if (quoteChecker==1){
-          strncat(line, &ch, 1);
-        }
-        else{
-          // Don't add commas if we are not in quotes
-          if (ch != ','){
-            strncat(line, &ch, 1);
-          }
-        }
-
-        // Check the column number of the header.
-        // You can change gitto any header
-        if (strcmp(line,Header)==0){
-          rowCol = count;
-        }
-        
-        // If the char is a comma and we are not in a quote print the string created so far for that entry
-        if (ch == ',' && quoteChecker == 0){
-          // count is counting the columns Ref_Date is 0th col 
-          if (count == rowCol){
-              if (strcmp(line, Header) != 0){
-                int temp = atoi(line); 
-                if (operation == 0){
-                  if(temp < minimum){
-                    minimum = temp;
-                  }
-                }
-                else if(operation == 1){
-                  if (temp > maximum){
-                    maximum = temp;
-                  }
-                }
-                else if(operation == 2){
-                  printf("%d\n",temp);
-                  meanTotal += temp;
-                  meanCount += 1;
-                }
-                else{
-                  return EXIT_FAILURE;
-                }
-                
-              }
-          }
-          count = count + 1;
-          strcpy(line, "");
-        }
-          
-          // If we find open quotes set quoteChecker to 1(true) so we dont consider any commas inside of them
-          if (ch == '\"' && quoteChecker == 0){
-            quoteChecker = 1;
-            continue;
-          }
-
-          // Found closing quotes so set it equal to false, next char is either a comma or a new line
-          if (ch == '\"' && quoteChecker == 1){
-            quoteChecker = 0;
-            continue;
-          }
-
-        // Reset string of line since it's a new line
-        if (ch == '\n'){
-            int size = strlen(line);
-            line[size - 2] = '\0';
-          // count is counting the columns Value is 0th col
-          // If the entry is the last of the line there is no comma it begins a new line so we check for a new line instead of comma
-          if (count == rowCol){
-            // printf("%s", line);
-            if (strcmp(line, Header) != 0){
-                int temp = atoi(line); 
-                if (operation == 0){
-                  if(temp < minimum){
-                    minimum = temp;
-                  }
-                }
-                else if(operation == 1){
-                  if (temp > maximum){
-                    maximum = temp;
-                  }
-                }
-                else if(operation == 2){
-                  meanTotal += temp;
-                  meanCount += 1;
-                }
-                else{
-                  return EXIT_FAILURE;
-                }
-          }
-          }
-          strcpy(line, "");
-          count = 0;
-        }
-
+  while ((ch = fgetc(filePointer)) != EOF){
+    //If we are in quotes add commas to string
+    if (quoteChecker==1){
+      strncat(line, &ch, 1);
+    }
+    else{
+      // Don't add commas if we are not in quotes
+      if (ch != ','){
+        strncat(line, &ch, 1);
       }
+    }
+    // Check the column number of the header.
+    // You can change gitto any header
+    if (strcmp(line,Header)==0){
+      rowCol = count;
+    }
+    // If the char is a comma and we are not in a quote print the string created so far for that entry
+    if (ch == ',' && quoteChecker == 0){
+      // count is counting the columns Ref_Date is 0th col 
+      if (count == rowCol){
+        if (strcmp(line, Header) != 0){
+          //printf("%s\n", line);
+          double temp = atof(line); 
+          if (operation == 0){
+            if(temp < minimum){
+              minimum = temp;
+            }
+          }
+          else if(operation == 1){
+            if (temp > maximum){
+              maximum = temp;
+            }
+          }
+          else if(operation == 2){
+            //printf("%f\n",temp);
+            meanTotal += temp;
+            meanCount += 1;
+          }
+          else{
+            return EXIT_FAILURE;
+          }
+        }
+      }
+      count = count + 1;
+      strcpy(line, "");
+    }
+    // If we find open quotes set quoteChecker to 1(true) so we dont consider any commas inside of them
+    if (ch == '\"' && quoteChecker == 0){
+      quoteChecker = 1;
+      continue;
+    }
+
+    // Found closing quotes so set it equal to false, next char is either a comma or a new line
+    if (ch == '\"' && quoteChecker == 1){
+      quoteChecker = 0;
+      continue;
+    }
+
+    // Reset string of line since it's a new line
+    if (ch == '\n'){
+        int size = strlen(line);
+        line[size-1] = '\0';
+      // count is counting the columns Value is 0th col
+      // If the entry is the last of the line there is no comma it begins a new line so we check for a new line instead of comma
+      if (count == rowCol){
+        // printf("%s", line);
+        if (strcmp(line, Header) != 0){
+            //printf("%s\n", line);
+            float temp = atof(line); 
+            trueTemp = temp;
+            if (operation == 0){
+              if(temp < minimum){
+                minimum = temp;
+              }
+            }
+            else if(operation == 1){
+              if (temp > maximum){
+                maximum = temp;
+              }
+            }
+            else if(operation == 2){
+              meanTotal += trueTemp;
+             // printf("%f\n", temp);
+              meanCount += 1;
+            }
+            else{
+              return EXIT_FAILURE;
+            }
+        }
+      }
+      strcpy(line, "");
+      count = 0;
+    }
+
+  }
   if(operation == 0){
     printf("%d\n",minimum);
   }
@@ -152,12 +151,11 @@ int minMaxMean(char* Header , int operation){
     printf("%d\n",maximum);
   }
   else if(operation == 2){
-    printf("%f\n", meanTotal);
-    printf("%d\n", meanCount);
+    //printf("%f\n", meanTotal);
+    //printf("%d\n", meanCount);
     mean = meanTotal/meanCount;
     printf("%f\n",mean);
   }
-  
   return 0;
 }
 
@@ -266,8 +264,6 @@ int main(int argc, char *argv[]) {
             minMaxMean(argv[3], 2);
           }
           else if(strcmp(argv[2],"-records") ==0){
-            printf("%s", argv[3]);
-            printf("%s", argv[4]);
             getRecords(argv[3],argv[4]);
           }
         }
